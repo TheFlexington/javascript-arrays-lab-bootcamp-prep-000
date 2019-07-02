611a06ab28832a08e5de6464dcc793d0dac1973b
Felix Martinez <felixaviermartinez@gmail.com>
2019-07-02 19:48:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(array) {
+  kittens.push("Ralph");
+  console.log(kittens);
+}
+
+function destructivelyPrependKitten(prepend) {
+  kittens.unshift("Bob");
+  console.log(kittens);
+}
+
+function destructivelyRemoveLastKitten(remove) {
+  kittens.pop();
+  console.log(kittens);
+}
+
+function destructivelyRemoveFirstKitten(removefirst) {
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
